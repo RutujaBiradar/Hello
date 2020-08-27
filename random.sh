5ad5ea7a812b5edfc159fdfe8375f9bbb0f12dec
RutujaBiradar <rutujabiradar596@gmail.com>
2020-08-27 14:39:03 -0400

    first commit

---
+++ random.sh
@@ -0,0 +1,12 @@
+#! /bin/bash 
+emp= $((RANDOM%2))
+if [$emp -eq 4]
+then
+	echo "employee has done part time job"
+elif [$h e]
+then
+	rate=20
+	h=8
+	salary=$((rate*h))
+	echo"employee full time salary is $salary"
+fi
